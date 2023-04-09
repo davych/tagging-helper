8b768023528403ea88a80e471587368897af8ecb
davy_chen <davy_chen@epam.com>
2023-04-09 20:37:50 +0800

    chore: enhance router case

--- example/src/routerSwitch/Page1.tsx
+++ example/src/routerSwitch/Page1.tsx
@@ -1,39 +1,9 @@
 import * as React from 'react';
-import {
-  useNavigate,
-  useLocation
-} from "react-router-dom";
-import { withTaggingClickable } from '../../../src/react'
-
-const Button = (props: any) => {
-  return <button {...props}>{props.children}</button>
-}
-
-const Icon = (props: any) => {
-  return <div {...props}>{props.children}</div>
-}
-
-const MyButton = withTaggingClickable(Button);
-const MyIcon = withTaggingClickable(Icon);
 
 export default function Page1() {
-  const navigate = useNavigate()
   return (
     <div>
       <h1>hello, this is page1</h1>
-      <MyButton
-        identifier="test"
-        onClick={() => {
-          console.log('click--------》')
-        }}
-      >test</MyButton>
-
-      <MyIcon
-        identifier="icon-test"
-        onClick={() => {
-          console.log('click--------》')
-        }}
-      >icon</MyIcon>
     </div>
   )
 }
